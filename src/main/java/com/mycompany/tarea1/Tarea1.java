6e5f382a0295169721849fa02dede889d1d46728
jose <jose@jose-VirtualBox>
2021-01-25 12:58:16 +0100

    corregido

--- src/main/java/com/mycompany/tarea1/Tarea1.java
+++ src/main/java/com/mycompany/tarea1/Tarea1.java
@@ -12,38 +12,37 @@ import java.io.InputStreamReader;
  * @author jose
  */
 public class Tarea1 {
-    String nombre;
+    
     public static void main(String[] args) throws IOException{
         Double  x, y,z;
         float a, b, c;
+        String nombre;
         
         BufferedReader bufer = new BufferedReader(new InputStreamReader(System.in));
         String entrada;
             
         System.out.println("Escribe el valor de x: ");
         entrada = bufer.readLine();
-        x= Double.parseInt(entrada);
+        x= Double.parseDouble(entrada);
         System.out.println("Escribe el valor de y: ");
         entrada = bufer.readLine();
         y = Double.parseDouble(entrada);
         z = y / x;
-        System.out.println("El residuo de dividir y ( + y + ") entre x ( " +x + ) es: " + z);
+        System.out.println("El residuo de dividir y (" + y + ") entre x ( " + x + ") es: " + z);
             
         System.out.println("Escribe el valor de a: ");
-        entrada = b.readLine();
-        a = entrada;
+        entrada = bufer.readLine();
+        a = Float.parseFloat(entrada);
         System.out.println("Escribe el valor de b: ");
-        in = bufer.readLine();
+        entrada = bufer.readLine();
         b = Float.parseFloat(entrada);
         c = a * b ;
         System.out.println("El resultado de multiplicar a por b es: " + c);
-            
-            
-            
+        
+        System.out.println("Escribe una letra: ");
+        entrada = bufer.readLine();
+        nombre = entrada;
+        System.out.println("El  nombre que escribiste es: " + nombre);                  
     }
-    System.out.println("Escribe una letra: ");
-    entrada = bufer.readLine();
-    nombre = Entrada.ChartAt(12);
-    System.out.println("El  nombre que escribiste es: " + Nombre);
 }
 
